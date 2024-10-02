a69faabcb59d0654c7ab67c37ce8e828d50fccc7
morciusX7 <amakorchiemela@gmail.com>
2024-10-02 18:55:42 +0100

    changes

--- components/auth/vendor-login-form.tsx
+++ components/auth/vendor-login-form.tsx
@@ -43,8 +43,8 @@ export const VendorLoginForm = () => {
 
     startTransition(() => {
       vendorLogin(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        setSuccess(data?.success);
       });
     });
   };
